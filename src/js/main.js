ce74e2246b45a405cb406b9239a50373ac0e3bd1
Marina Moroz <moroz2387moroz@yandex.ru>
2024-06-05 12:48:14 +1000

    add label wave

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
-
+window.addEventListener("DOMContentLoaded", () => {
+  const labels = document.querySelectorAll(".form-control label");
+  labels.forEach((label) => {
+    label.innerHTML = label.innerText
+      .split("")
+      .map(
+        (letter, idx) =>
+          `<span style="transition-delay: ${idx * 50}ms">${letter}</span>`
+      )
+      .join("");
+  });
+});
